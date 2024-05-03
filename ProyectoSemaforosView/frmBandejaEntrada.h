c14ee6e43011bfe474dcc5d2b5da15f0d39f26c0
alulab14 <alulab14@V20825>
2024-05-02 20:04:31 -0500

    hola

--- ProyectoSemaforosView/frmBandejaEntrada.h
+++ ProyectoSemaforosView/frmBandejaEntrada.h
@@ -76,11 +76,12 @@ namespace ProyectoSemaforosView {
 				this->Column1,
 					this->Column2, this->Column3
 			});
-			this->dataGridView1->Location = System::Drawing::Point(41, 161);
+			this->dataGridView1->Location = System::Drawing::Point(27, 105);
+			this->dataGridView1->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->dataGridView1->Name = L"dataGridView1";
 			this->dataGridView1->RowHeadersWidth = 62;
 			this->dataGridView1->RowTemplate->Height = 28;
-			this->dataGridView1->Size = System::Drawing::Size(965, 150);
+			this->dataGridView1->Size = System::Drawing::Size(643, 98);
 			this->dataGridView1->TabIndex = 0;
 			// 
 			// Column1
@@ -107,39 +108,44 @@ namespace ProyectoSemaforosView {
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(54, 47);
+			this->label1->Location = System::Drawing::Point(36, 31);
+			this->label1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(68, 20);
+			this->label1->Size = System::Drawing::Size(46, 13);
 			this->label1->TabIndex = 1;
 			this->label1->Text = L"Usuario:";
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(298, 469);
+			this->button1->Location = System::Drawing::Point(199, 305);
+			this->button1->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(136, 36);
+			this->button1->Size = System::Drawing::Size(91, 23);
 			this->button1->TabIndex = 2;
 			this->button1->Text = L"Enviar mensaje";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &frmBandejaEntrada::button1_Click);
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(553, 469);
+			this->button2->Location = System::Drawing::Point(369, 305);
+			this->button2->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(136, 36);
+			this->button2->Size = System::Drawing::Size(91, 23);
 			this->button2->TabIndex = 2;
 			this->button2->Text = L"Salir";
 			this->button2->UseVisualStyleBackColor = true;
 			// 
 			// frmBandejaEntrada
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1058, 564);
+			this->ClientSize = System::Drawing::Size(705, 367);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->dataGridView1);
+			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->Name = L"frmBandejaEntrada";
 			this->Text = L"frmBandejaEntrada";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
@@ -148,5 +154,8 @@ namespace ProyectoSemaforosView {
 
 		}
 #pragma endregion
-	};
+		//holaDSÑ;LVMSSOVSMVMS______________________________________________//
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
 }
